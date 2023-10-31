32b03666095344b07936918fd58b7095c39aecb5
Mereki <143884406+Mereki@users.noreply.github.com>
2023-10-31 13:52:13 -0500

    Update sum_squares.py

--- sum_squares.py
+++ sum_squares.py
@@ -11,24 +11,10 @@ def list_nums(n):
                 nc = nb - c**2
                 d = floor(sqrt(nc))
                 if d**2 == nc:
-                    return [a, b, c, d]
-def count_sets(n):
-    count = 0 # basically same thing as list_nums but not returning first set
-    for a in range(floor(sqrt(n))+1):
-        na = n - a**2
-        for b in range(a, floor(sqrt(na))+1):
-            nb = na - b**2
-            for c in range(b, floor(sqrt(nb))+1):
-                nc = nb - c**2
-                d = floor(sqrt(nc))
-                if d**2 == nc:
-                    count += 1
-    return count - 1       
+                    return [a, b, c, d]  
     
 # how to measure how long your function takes to run:
 t1 = time()  # get start time
 print(list_nums(512))  # run function
 t2 = time()  # get end time
 print(f"This took {t2 - t1} seconds")  # print result
-
-print(count_sets(16)) # [0,0,0,4] or [2,2,2,2]
