35892e741aedeee166eedbcc74ac3a3a2ca229b0
Mereki <143884406+Mereki@users.noreply.github.com>
2023-10-31 13:42:54 -0500

    Update sum_squares.py

--- sum_squares.py
+++ sum_squares.py
@@ -1,6 +1,7 @@
 from time import time
 from math import *
 
+# d >= c >= b >= a
 def list_nums(n):
     for a in range(floor(sqrt(n))+1):
         na = n - a**2
@@ -11,7 +12,19 @@ def list_nums(n):
                 d = floor(sqrt(nc))
                 if d**2 == nc:
                     return [a, b, c, d]
-
+def count_sets(n):
+    count = 0 # basically same thing as list_nums but not returning first set
+    for a in range(floor(sqrt(n))+1):
+        na = n - a**2
+        for b in range(a, floor(sqrt(na))+1):
+            nb = na - b**2
+            for c in range(b, floor(sqrt(nb))+1):
+                nc = nb - c**2
+                d = floor(sqrt(nc))
+                if d**2 == nc:
+                    count += 1
+    return count              
+    
 # how to measure how long your function takes to run:
 t1 = time()  # get start time
 print(list_nums(512))  # run function
